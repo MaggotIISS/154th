07a1149805ffbfcd1b074e40bb203bd7abc4c272
Mark Ferguson <maggot.iiss@sky.com>
2017-10-12 00:22:55 +0100

    Initial commit

---
+++ src/pkg154th/BRS154thController.java
@@ -0,0 +1,37 @@
+/*
+ To change this license header, choose License Headers in Project Properties.
+ To change this template file, choose Tools | Templates
+ and open the template in the editor.
+ */
+package pkg154th;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ FXML Controller class
+
+ @author Mark Ferguson
+ */
+public class BRS154thController implements Initializable {
+
+  @FXML
+  private AnchorPane AnchorPane;
+
+  /**
+   Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }
+
+  @FXML
+  private void picClick(MouseEvent event) {
+  }
+
+}
